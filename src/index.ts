8c58b329df5ade6f8212f56941d337e591b11d82
pandu <pandu@nexacu.com.au>
2023-12-08 14:59:13 +0700

    add migration: user table

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
 const app = express();
+
+app.use(express.json());
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
